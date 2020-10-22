50d5c8e053103c3b07af268e7e772c1db4778702
oetyng <oetyng@gmail.com>
2020-10-22 22:12:26 +0200

    refactor: split transfer to debit+credit

--- src/messaging/cmd.rs
+++ src/messaging/cmd.rs
@@ -7,8 +7,9 @@
 // specific language governing permissions and limitations relating to use of the SAFE Network
 // Software.
 
+use super::super::transfer::TransferAgreementProof;
 use super::{auth::AuthCmd, data::DataCmd, transfer::TransferCmd, AuthorisationKind};
-use crate::{DebitAgreementProof, XorName};
+use crate::XorName;
 use serde::{Deserialize, Serialize};
 
 /// TODO: docs
@@ -22,7 +23,7 @@ pub enum Cmd {
         ///
         cmd: DataCmd,
         ///
-        payment: DebitAgreementProof,
+        payment: TransferAgreementProof,
     },
     ///
     Transfer(TransferCmd),
